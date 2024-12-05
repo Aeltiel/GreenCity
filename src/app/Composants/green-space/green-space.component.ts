2ee679caac16f037e1861f9ea4e51bc582e445d9
Ingrid Stemer <ingrid.stemer@live.fr>
2024-12-05 10:55:08 +0100

    Feat : prepare files dans folder for the green space features

---
+++ src/app/Composants/green-space/green-space.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-green-space',
+  imports: [],
+  templateUrl: './green-space.component.html',
+  styleUrl: './green-space.component.scss'
+})
+export class GreenSpaceComponent {
+
+}
