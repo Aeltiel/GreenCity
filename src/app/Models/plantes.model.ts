aceb8200d52daf8ab8ebfab516706a741d14c5aa
Ingrid Stemer <ingrid.stemer@live.fr>
2024-12-06 15:54:07 +0100

    feat: ajout des plantes dans le json et ajout du model plantes dans les Models

---
+++ src/app/Models/plantes.model.ts
@@ -0,0 +1,10 @@
+export interface GreenSpace {
+  id: string;
+  nomCommun: string;
+  nomScientifique: string;
+  typeDeSol: string;
+  expositionAuSoleil: string;
+  frequenceArrosage: string;
+  saisonDePlantation: string;
+  saisonDeRecolte: string;
+}
