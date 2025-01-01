a7bdea67ab0ab455f08790e0bfba9730e2832851
Aeltiel <ingrid.stemer@live.fr>
2025-01-01 17:04:15 +0100

    feat : ajout d'un header + ajoput d'une page pour tous les espaces verts

---
+++ src/app/Pages/green-spaces-pages/green-spaces-pages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-green-spaces-pages',
+  imports: [],
+  templateUrl: './green-spaces-pages.component.html',
+  styleUrl: './green-spaces-pages.component.scss'
+})
+export class GreenSpacesPagesComponent {
+
+}
